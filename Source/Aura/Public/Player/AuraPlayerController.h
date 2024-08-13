9d3d24a451a25c7d9d6bc9e26aa755d2fe0f18a0
kryzel <correspondence.nu@gmail.com>
2024-08-13 22:28:24 +0530

    2-8

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,27 @@
+// Copyright Udemy Guy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerController() ;
+protected : 
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputMappingContext>AuraContext;
+
+};
